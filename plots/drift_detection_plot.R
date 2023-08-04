753ffcf8f4e912a296b96d2312ccffd7e162fdb1
gabrieljaguiar <gja.gbrl@gmail.com>
2023-08-04 09:56:18 -0400

    new_plots

--- plots/drift_detection_plot.R
+++ plots/drift_detection_plot.R
@@ -3,15 +3,17 @@ library (reshape2)
 
 classifiers = c("LB", "HT", "NB")
 dds = c("ADWIN", "DDM", "RDDM")
-stream = c("rt_10", "rt_5","rf_10", "rf_5")
-
-
+stream = 
+  c("no_imbalance_switching_rf_5_sudden",
+  "no_imbalance_switching_rf_5_gradual",
+  "imbalance_switching_rf_5_sudden", 
+  "imbalance_switching_rf_5_gradual")
 
 dd_data <- data.frame()
 for (dd in dds){
   for (c in classifiers) {
       for (s in stream) {
-        file_name = paste0(c, "_", dd, "_stable_", s, ".csv")
+        file_name = paste0(c, "_", dd, "_", s, ".csv")
         path_file = paste0("../exp_output/", file_name)
         data <- read.csv(path_file)
         data <- data[, c("idx", "drifts_alerts")]
@@ -30,7 +32,7 @@ dd_data <- dd_data[dd_data$drifts_alerts == 1,]
 ggplot(data = dd_data, aes(x=idx, y = stream)) + 
   geom_point(shape=15, color="red") + 
   scale_x_continuous(limits=c(0,100000)) +
-  geom_vline(xintercept = c(30000, 60000), color="black", alpha=0.8, linetype=3) +
+  geom_vline(xintercept = c(20000, 40000, 60000, 80000), color="black", alpha=0.8, linetype=3) +
   theme_bw() +
   theme(
     panel.grid.major.x = element_blank(),
--- plots/g_mean_plot.R
+++ plots/g_mean_plot.R
@@ -2,32 +2,39 @@ library (ggplot2)
 library (reshape2)
 
 classifiers = c("LB", "HT", "NB")
-dds = c("ADWIN", "DDM", "RDDM")
-stream = c("rt_10", "rt_5","rf_10", "rf_5")
+dds = c("ADWIN")
+stream =  c("no_imbalance_switching_rf_5_sudden",
+    "no_imbalance_switching_rf_5_gradual",
+    "imbalance_switching_rf_5_sudden", 
+    "imbalance_switching_rf_5_gradual")
 
 for (c in classifiers) {
   for (dd in dds) {
     for (s in stream) {
-      file_name = paste0(c, "_", dd, "_stable_", s, ".csv")
+      file_name = paste0(c, "_", dd, "_", s, ".csv")
       path_file = paste0("../exp_output/", file_name)
+      data <- read.csv(path_file)
+      
+      data <- data[, 2:(ncol(data) - 1)]
+      
+      data_melt <- melt(data, id.vars = c("idx"))
+      data_melt$local <- "local"
+      data_melt[data_melt$variable=="accuracy",]$local <- "global"
+      data_melt[data_melt$variable=="gmean",]$local <- "global"
+      
+      g <- ggplot(data_melt, aes(x=idx, y=value)) + 
+        geom_line(aes(color=variable)) + 
+        theme_bw() +
+        theme(
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank()
+        ) +
+        geom_vline(xintercept = c(20000, 40000, 60000, 80000), color="black", alpha=0.8, linetype=3) +
+        facet_grid(.~local)
+      
+      ggsave(paste0(file_name,".pdf"), g, width = 11)
     }
   }
 }
 
-data <- read.csv("../exp_output/LB_ADWIN_stable_rt_5.csv")
 
-data <- data[, 2:(ncol(data) - 1)]
-
-data_melt <- melt(data, id.vars = c("idx"))
-data_melt$local <- "local"
-data_melt[data_melt$variable=="G.Mean",]$local <- "global"
-
-ggplot(data_melt, aes(x=idx, y=value)) + 
-  geom_line(aes(color=variable)) + 
-  theme_bw() +
-  theme(
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  ) +
-  geom_vline(xintercept = c(30000, 60000), color="black", alpha=0.8, linetype=3) +
-  facet_grid(.~local)
