71b30f4162a435d30796c19ac8b9a1850d4c26fc
gabrieljaguiar <gja.gbrl@gmail.com>
2023-09-16 17:17:13 -0400

    FIX

--- metrics/concept_drift_metrics.py
+++ metrics/concept_drift_metrics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from glob import glob
-
+from tqdm import tqdm
 # drift_alerts_HT_ADWIN_no_drift_rbf_c_2_f_5_1_1
 # HT_RDDM_single_class_local_splitting_cluster_ds_1_c_10_ca_1_f_2_1_1
 
@@ -10,12 +10,16 @@ TP_WINDOW = 5000
 classifiers = ["HT"]
 
 dds = [
-    "ADWIN",
-    "PageHinkley",
-    "HDDM",
-    "KSWIN",
-    "DDM",
-    "RDDM",
+    #"ADWIN",
+    #"PageHinkley",
+    #"HDDM",
+    #"KSWIN",
+    #"DDM",
+    #"RDDM",
+    #"STEPD",
+    #"GMA",
+    "ECDD",
+    "EDDM"
 ]
 
 scenarios = [
@@ -38,8 +42,10 @@ for scenario in scenarios:
                     for path, subdir, files in os.walk(PATH)
                     for file in glob(os.path.join(path, EXT))
                 ]
+                print (scenario)
+                print (dd)
                 if scenario != "no_drift":
-                    for file in streams:
+                    for idx, file in tqdm(enumerate(streams), total=len(streams)):
                         splited_name = file.split("_")
                         index_base = splited_name.index(scenario.split("_")[-1])
                         index_ds = splited_name.index("ds") + 1
@@ -60,6 +66,7 @@ for scenario in scenarios:
                         fn = 0
                         
                         for _, row in df.iterrows():
+                            #print (row)
                             drift_idx = row["idx"]
                             if (drift_idx >= drift_position and drift_idx <= drift_position + TP_WINDOW):
                                 if tp == 0:
